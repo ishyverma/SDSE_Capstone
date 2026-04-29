6cdcf9419390edfe626f3c3814aa1a25d9f425a0
ishyverma <ishyamver@gmail.com>
2026-04-29 13:13:29 +0530

    changed package.json

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,6 +7,8 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient()
+  new PrismaClient({
+    datasourceUrl: "file:./dev.db"
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
